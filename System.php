5b76a098ba5648c32b4144a1276344f25e9a22a6
nelcoa <nelcoa@users.noreply.github.com>
2016-05-20 01:40:01 +0200

    Add files via upload

---
+++ System.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * OpenFireRestAPI is based entirely on official documentation of the REST API
+ * Plugin and you can extend it by following the directives of the documentation
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/nelcoa/PHPOpenFireRestAPI/contributors
+ *
+ * @author Luca Agnello <lcagnello@gmail.com>
+ * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html
+ */
+
+namespace OpenFireRestAPI;
+
+use OpenFireRestAPI\Dispatcher\Method;
+use OpenFireRestAPI\Dispatcher\Dispatcher;
+
+/**
+ * System related REST Endpoints
+ * Class System
+ * @package OpenFireRestAPI
+ * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#system-related-rest-endpoints
+ */
+class System extends Dispatcher
+{
+    public static $endpoint = '/system';
+
+    /**
+     * Get all system properties
+     * @return array with System properties
+     * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#retrieve-all-system-properties
+     */
+    public static function retrieveAllSystemProperties()
+    {
+        $endpoint = self::$endpoint . '/properties';
+        return self::sendRequest(Method::GET, $endpoint);
+    }
+
+    /**
+     * Get information over specific system property
+     * @param $propertyName
+     * @return array with System property
+     * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#retrieve-system-property
+     */
+    public static function retrieveSystemProperty($propertyName)
+    {
+        $endpoint = self::$endpoint . '/properties/' . $propertyName;
+        return self::sendRequest(Method::GET, $endpoint);
+    }
+
+    /**
+     * Create a system property
+     * @param $propertyName
+     * @param $propertyValue
+     * @return array with HTTP status 201 (Created)
+     * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#create-a-system-property
+     */
+    public static function createSystemProperty($propertyName, $propertyValue)
+    {
+        $endpoint = self::$endpoint . '/properties';
+        return self::sendRequest(Method::POST, $endpoint, compact('propertyName', 'propertyValue'));
+    }
+
+    /**
+     * Delete a system property
+     * @param $propertyName
+     * @return array with HTTP status 200 (OK)
+     * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#delete-a-system-property
+     */
+    public static function deleteSystemProperty($propertyName)
+    {
+        $endpoint = self::$endpoint . '/properties/' . $propertyName;
+        return self::sendRequest(Method::DELETE, $endpoint);
+    }
+
+    /**
+     * Update a system property
+     * @param $propertyName
+     * @return array with HTTP status 200 (OK)
+     * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#update-a-system-property
+     */
+    public static function updateSystemProperty($propertyName)
+    {
+        $endpoint = self::$endpoint . '/properties/' . $propertyName;
+        return self::sendRequest(Method::PUT, $endpoint);
+    }
+
+    /**
+     * Get count of concurrent sessions
+     * @return array with Sessions count
+     * @link http://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#retrieve-concurrent-sessions
+     */
+    public static function retrieveConcurrentSessions()
+    {
+        $endpoint = self::$endpoint . '/statistics/sessions';
+        return self::sendRequest(Method::GET, $endpoint);
+    }
+}
