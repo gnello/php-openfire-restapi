3f60bfc778ae74d7cf0cb1497c3ac577a39cb502
Luca Agnello <lcagnello@gmail.com>
2016-05-23 01:45:28 +0200

    .

---
+++ src/Payload/ChatRoom.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * OpenFireRestAPI is based entirely on official documentation of the REST API
+ * Plugin and you can extend it by following the directives of the documentation
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/nelcoa/PHPOpenFireRestAPI/contributors
+ *
+ * @author Luca Agnello <lcagnello@gmail.com>
+ * @link https://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html
+ */
+
+namespace Nelcoa\OpenFireRestAPI\Payload;
+
+use Nelcoa\OpenFireRestAPI\Payload\Model\IChatRoom;
+
+/**
+ * Payload of Group related REST Endpoint
+ * Class ChatRoom
+ * @package Nelcoa\OpenFireRestAPI\Payload
+ * @link http://www.igniterealtime.org/projects/openfire/plugins/restapi/readme.html#chat-room-related-rest-endpoints
+ */
+class ChatRoom extends Payload implements IChatRoom
+{
+    //TODO: add @var + @methods
+}
